0ecf4c522f94116a7751847009af7b78f4150159
Rain-Drops02 <57630390+Rain-Drops02@users.noreply.github.com>
2020-04-28 19:04:53 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.menu-wrap').click(function(){
+      $('#login-show-wrap').fadeIn();
+    });
+
+    $('#close-login-show').click(function(){
+      $('#login-show-wrap').fadeOut();
+      
+    })
+    
+  });
